97a6fbcf530f59d9c4a8e2b5f314b3e84d3ce236
Atarico <luiz_paulo.09@hotmail.com>
2015-07-21 19:37:29 -0300

    omgimsodumb
    this /\

---
+++ Assets/moveDisBitch.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveDisBitch : MonoBehaviour {
+
+
+	public int velocidade = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("d")){
+			transform.Translate(new Vector3(velocidade*Time.deltaTime,0,0));
+		}
+		if(Input.GetKey("a")){
+			transform.Translate(new Vector3(-velocidade*Time.deltaTime,0,0));
+		}
+	}
+}
