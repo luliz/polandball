6d02ae30a8f914bf52c5f7f4d99a5556360605cc
Atarico <luiz_paulo.09@hotmail.com>
2015-07-30 18:51:34 -0300

    sadada
    holyfk
    
    omg

--- Assets/moveDisBitch.cs
+++ Assets/moveDisBitch.cs
@@ -5,6 +5,9 @@ public class moveDisBitch : MonoBehaviour {
 
 
 	public int velocidade = 1;
+	private bool ground = false;
+	private bool right = true;
+	public GameObject checkGround;
 
 	// Use this for initialization
 	void Start () {
@@ -19,5 +22,9 @@ public class moveDisBitch : MonoBehaviour {
 		if(Input.GetKey("a")){
 			transform.Translate(new Vector3(-velocidade*Time.deltaTime,0,0));
 		}
+		if (Input.GetKeyDown ("w")) {
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 2.5f), ForceMode2D.Impulse);
+		}
 	}
+
 }
