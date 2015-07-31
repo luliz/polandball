9c88bf22651af1989b4cbf191872bf0a4a7fe41c
Luccas Mateus <Luccasmmg@gmail.com>
2015-07-31 17:51:05 -0300

    Agora ta certo

--- Assets/checkGround.cs
+++ Assets/checkGround.cs
@@ -2,19 +2,26 @@
 using System.Collections;
 
 public class checkGround : MonoBehaviour {
-
+	
+	public bool ground = false;
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 	void OnTriggerEnter2D (Collider2D col){
-		if(col.gameObject.name == "Chao"){
-			Debug.Log("Ta no chao");
+		if(col.gameObject.name == "ground"){
+			ground = true;
+		}
+	}
+	void OnTriggerExit2D (Collider2D col){
+		if (col.gameObject.name == "ground") {
+			ground = false;
 		}
 	}
 }
--- Assets/moveDisBitch.cs
+++ Assets/moveDisBitch.cs
@@ -2,29 +2,44 @@
 using System.Collections;
 
 public class moveDisBitch : MonoBehaviour {
-
-
+	
+	
 	public int velocidade = 1;
-	private bool ground = false;
 	private bool right = true;
-	public GameObject checkGround;
-
+	private bool ground = true;
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		ground = transform.GetComponentInChildren<checkGround>().ground;
 		if(Input.GetKey("d")){
 			transform.Translate(new Vector3(velocidade*Time.deltaTime,0,0));
+			if(!right){
+				Flip();
+			}
 		}
 		if(Input.GetKey("a")){
 			transform.Translate(new Vector3(-velocidade*Time.deltaTime,0,0));
+			if(right){
+				Flip();
+			}
 		}
-		if (Input.GetKeyDown ("w")) {
+		if (Input.GetKeyDown ("w") && ground) {
 			GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 2.5f), ForceMode2D.Impulse);
 		}
 	}
-
+	private void Flip(){
+		// Troca pra onde o player ta olhando.
+		right = !right;
+		
+		// Vira o player .
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+	
 }
