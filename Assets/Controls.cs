614b08571d9ac8f180afe7e46c75c99632959700
Lulinha <luiz.viana000@hotmail.com>
2015-08-08 08:53:15 -0300

    rope update

---
+++ Assets/Controls.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Controls : object {
+
+	public static KeyCode up = KeyCode.W;
+	public static KeyCode down = KeyCode.S;
+	public static KeyCode walkLeft = KeyCode.A;
+	public static KeyCode walkRight = KeyCode.D;
+	public static KeyCode jump = KeyCode.Space;
+}
--- Assets/moveDisBitch.cs
+++ Assets/moveDisBitch.cs
@@ -1,14 +1,14 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class moveDisBitch : MonoBehaviour {
 	
 	
-	public int velocidade = 1;
+	public float velocidade = 1f;
 	private bool right = true;
 	private bool ground = true;
 
-	private Rigidbody2D myRigidBody2D;
+	public Rigidbody2D myRigidBody2D;
 
 	public bool onRope;
 	public float climbSpeed;
@@ -24,36 +24,43 @@ public class moveDisBitch : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		ground = transform.GetComponentInChildren<checkGround>().ground;
-		if(Input.GetKey("d")){
-			transform.Translate(new Vector3(velocidade*Time.deltaTime,0,0));
-			if(!right){
-				Flip();
-			}
-		}
-		if(Input.GetKey("a")){
-			transform.Translate(new Vector3(-velocidade*Time.deltaTime,0,0));
-			if(right){
-				Flip();
-			}
-		}
-		if (Input.GetKeyDown (KeyCode.Space) && ground && !onRope) {
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 2.5f), ForceMode2D.Impulse);
-		}
-		if (onRope) {
-			myRigidBody2D.gravityScale = 0f;
-			if(Input.GetKey("w")){
-				transform.Translate(new Vector3(0,velocidade*Time.deltaTime,0));
 
+		ground = transform.GetComponentInChildren<checkGround>().ground;
+
+		if (onRope) {
+			myRigidBody2D.gravityScale = 0;
+			myRigidBody2D.velocity = Vector2.zero;
+			if(Input.GetKey(Controls.up)){
+				myRigidBody2D.velocity = new Vector2(myRigidBody2D.velocity.x,velocidade);
+				
 			}
-			if(Input.GetKey("s")){
-				transform.Translate(new Vector3(0,-velocidade*Time.deltaTime,0));
+			if(Input.GetKey(Controls.down)){
+				myRigidBody2D.velocity = new Vector2(myRigidBody2D.velocity.x,-velocidade);
 				
 			}
 		}
 		if (!onRope) {
 			myRigidBody2D.gravityScale = gravityStore;
 		}
+		
+		if(Input.GetKey(Controls.walkRight)){
+			//transform.Translate(new Vector3(velocidade*Time.deltaTime,0,0)); Jeito de Paulao
+			myRigidBody2D.velocity = new Vector2(velocidade, myRigidBody2D.velocity.y);
+			if(!right){
+				Flip();
+			}
+		}
+		if(Input.GetKey(Controls.walkLeft)){
+			//transform.Translate(new Vector3(-velocidade*Time.deltaTime,0,0));
+			myRigidBody2D.velocity = new Vector2 (-velocidade, myRigidBody2D.velocity.y);
+			if(right){
+				Flip();
+			}
+		}
+		if (Input.GetKeyDown (Controls.jump) && ground) {
+			myRigidBody2D.AddForce(new Vector2(0f, 2.5f), ForceMode2D.Impulse);
+		}
+
 	}
 	private void Flip(){
 		// Troca pra onde o player ta olhando.
