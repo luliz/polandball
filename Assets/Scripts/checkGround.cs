7cd36af9990aec1592d2cf2d139d435b7ddbd01e
Lulinha <luiz.viana000@gmail.com>
2015-08-09 11:01:57 -0300

    Pastas organizadas

---
+++ Assets/Scripts/checkGround.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class checkGround : MonoBehaviour {
+	
+	public bool ground = false;
+	private Animator playerAnimator;
+	// Use this for initialization
+	void Start () {
+		playerAnimator = GetComponentInParent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerStay2D (Collider2D col){
+		if(col.gameObject.tag == "Jumpable"){
+			ground = true;
+			playerAnimator.SetBool("ground", true);
+		}
+	}
+	void OnTriggerExit2D (Collider2D col){
+		if (col.gameObject.tag == "Jumpable") {
+			ground = false;
+			playerAnimator.SetBool("ground", false);
+		}
+	}
+}
